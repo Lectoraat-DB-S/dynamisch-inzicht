ce86226b72db9372b3ee2721dcf9aca72efc69b5
Steven <steven.coenraats@windesheim.nl>
2022-05-25 16:35:01 +0200

    format

--- src/backend/importdb.php
+++ src/backend/importdb.php
@@ -10,19 +10,12 @@ try {
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $stmt = $conn->prepare("SELECT organisation_name, location FROM organisatie");
   $stmt->execute();
-  
-  
-  $result = $stmt->fetch();
+
+
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
   print_r($result);
-  
-
-
-
-}
-
- catch(PDOException $e) {
+} catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 };
 
 $conn = null;
-?> 
