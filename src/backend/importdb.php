d37348f1301354a037fd723e1d4316edd7961811
Steven <steven.coenraats@windesheim.nl>
2022-05-25 15:33:48 +0200

    db connection test

---
+++ src/backend/importdb.php
@@ -0,0 +1,28 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=dinwh", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT organisation_name, location FROM organisatie");
+  $stmt->execute();
+  
+  
+  $result = $stmt->fetch();
+  print_r($result);
+  
+
+
+
+}
+
+ catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+};
+
+$conn = null;
+?> 
