b0e5bf3ee5339d453b86ec3bf9a38418872c499f
Steven <steven.coenraats@windesheim.nl>
2022-06-07 13:00:59 +0200

    leaflet map + marker

---
+++ src/components/worldMap.tsx
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import '../style/WorldMap.css';
+
+export default class WorldMap extends Component{
+    
+
+    public render = () =>{
+        const perron038: [number, number] = [52.50563888516816, 6.08692708711251]
+        return(
+            <>
+            <div>
+    <MapContainer center={[52.50563888516816, 6.08692708711251]} zoom={15}scrollWheelZoom={true}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={perron038}>
+    <Popup>
+        <b>Perron038</b><br/>
+         Maak de toekomst
+      </Popup>
+    </Marker>
+    </MapContainer>
+    </div>
+    </>
+        )
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {createRoot} from 'react-dom/client'
 import './style/index.css';
 import List from './components/List';
+import WorldMap from './components/worldMap';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
 root.render(
   <React.StrictMode>
-      <List />
+      <WorldMap />
   </React.StrictMode>,
 );
