5bdf6587ff65cdd3922bc3b9d5ba699dae75edfb
Steven <steven.coenraats@windesheim.nl>
2022-06-07 14:36:57 +0200

    installed gridlex

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import WorldMap from "./components/worldMap";
 import Themes from "./components/themes";
+import "../src/style/App.css"
 
-export default class App extends Component{
-    public render = () =>{
-        return(
-            <>
-            <Themes/>
-            <WorldMap/>
-            </>
-        )
-    }
+export default class App extends Component {
+  public render = () => {
+    return (
+      <>
+        <div className="col-4">
+          <Themes />
+        </div>
+        <div className="col-8">
+          <WorldMap />
+        </div>
+      </>
+    );
+  };
 }
