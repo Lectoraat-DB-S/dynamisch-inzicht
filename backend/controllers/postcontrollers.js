aa46171b0fc6739c652ecfe1aa61c4d6a82e3b80
Steven <steven.coenraats@windesheim.nl>
2022-06-10 09:42:04 +0200

    perm check

--- backend/controllers/postcontrollers.js
+++ backend/controllers/postcontrollers.js
@@ -17,3 +17,4 @@ exports.getAllThemes = async (req, res, next) => {
 // exports.GetThemeById = async (req, res, next) => {
 //     res.send("get all themes by id")
 // }
+// can i still do something???
